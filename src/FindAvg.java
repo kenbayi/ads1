46ce861c165a6e8db5285db3a77bd2d3caa4f248
Ilyas <fortwink88@gmail.com>
2024-04-05 11:51:09 +0500

    commenting task 1 and 2

--- src/FindAvg.java
+++ src/FindAvg.java
@@ -1,4 +1,12 @@
 public class FindAvg {
+    /**
+     * This method used for calculate average value from the array.
+     * It is just iteration through array.
+     * adding each value to variable and dividing it to length of array.
+     * Time complexity: O(n), where n is length of array.
+     * @param arr is array which is come from Main.java file.
+     * @return the average value from the array.
+     */
     public double findAvg(int[] arr){
         int sum = 0;
         for(int i : arr){
--- src/FindMin.java
+++ src/FindMin.java
@@ -1,4 +1,12 @@
 public class FindMin {
+    /**
+     * This method used for calculate minimum value from the array.
+     * It is just iteration through array.
+     * and comparing each value from the array.
+     * Time complexity: O(n), where n is length of array.
+     * @param arr is array which is come from Main.java file.
+     * @return the minimum value from the array.
+     */
    public int findMin(int[] arr){
        int min = arr[0];
        for (int i = 1; i < arr.length; i++) {
