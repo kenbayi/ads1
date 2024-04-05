3d7c92d17a0e1fca5b27e051a82169c47c3a887b
Ilyas <fortwink88@gmail.com>
2024-04-05 12:25:11 +0500

    main

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,18 @@ public class Main {
         IsPrime isPrime = new IsPrime(); //calling class to use method isPrime
         FindMin findMin = new FindMin(); //calling class to use method find minimum
         FindAvg findAvg = new FindAvg(); //calling class to use method find average
+        Factorial factorial = new Factorial(); //calling class to use method factorial
         Scanner sc = new Scanner(System.in);
 
-        //to test 3rd task
+        //to test 3rd and 4th task
         int num = sc.nextInt();
         System.out.println("The number is " + ((isPrime.isPrime(num)) ? "Prime" : "Composite"));
+        System.out.println("The factorial is " + factorial.factorial(num));
 
         //to test 1st and 2nd task
         int n = sc.nextInt();
+
+        //declaring array and input numbers to array
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
