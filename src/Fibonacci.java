87a5e343985a289d4fda9eadb425d0b211904493
Ilyas <fortwink88@gmail.com>
2024-04-08 23:57:14 +0500

    commenting

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,8 +1,14 @@
 public class Fibonacci {
+    /**
+     * This method calculates the nth Fibonacci number recursively.
+     * It sums up the (n-1)th and (n-2)th Fibonacci numbers to calculate the nth Fibonacci number.
+     * Time complexity: Exponential, O(2^n), as it recursively calculates Fibonacci numbers.
+     * @param n The index of the Fibonacci number to be calculated.
+     * @return The nth Fibonacci number.
+     */
     public int fibonacci(int n){
         if(n <= 1)
             return n;
-
         return fibonacci(n - 1) + fibonacci(n - 2);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -7,13 +8,14 @@ public class Main {
         FindAvg findAvg = new FindAvg(); //calling class to use method find average
         Factorial factorial = new Factorial(); //calling class to use method factorial
         Fibonacci fibonacci = new Fibonacci(); //calling class to use method fibonacci
+        ReverseArray reverse = new ReverseArray(); //calling class to use method reverseArr
         Scanner sc = new Scanner(System.in);
 
         //to test 3rd and 4th task
         int num = sc.nextInt();
-        System.out.println("The number is " + ((isPrime.isPrime(num)) ? "Prime" : "Composite"));
-        System.out.println("The factorial is " + factorial.factorial(num));
-        System.out.println("Fibonacci sequence value is " + fibonacci.fibonacci(num));
+        System.out.println("The number is: " + ((isPrime.isPrime(num)) ? "Prime" : "Composite"));
+        System.out.println("The factorial is: " + factorial.factorial(num));
+        System.out.println("Fibonacci sequence value is: " + fibonacci.fibonacci(num));
 
         //to test 1st and 2nd task
         int n = sc.nextInt();
@@ -25,5 +27,7 @@ public class Main {
         }
         System.out.println("Minimum from the array is: " + findMin.findMin(arr));
         System.out.println("Average value from the arrays is: " + findAvg.findAvg(arr));
+        reverse.reverseArr(arr);
+        System.out.println("Reversed array: " + Arrays.toString(arr));
     }
 }
